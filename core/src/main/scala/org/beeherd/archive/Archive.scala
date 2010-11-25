8ddaf96c552f21eeb85e84db146203c33a97eaf8
samuel <samuel@samuel-desktop.(none)>
2010-11-25 10:21:38 -0600

    Fix copy-n-paste license

--- core/src/main/scala/org/beeherd/archive/Archive.scala
+++ core/src/main/scala/org/beeherd/archive/Archive.scala
@@ -1,11 +1,9 @@
 /**
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements. See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership. The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-  * "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at
+* Copyright 2010 Samuel Cox
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
 *
 * http://www.apache.org/licenses/LICENSE-2.0
 *
@@ -20,7 +18,6 @@ package org.beeherd.zip
 
 import java.io.File
 import java.util.regex._
-import java.util.zip._
 
 /**
  *
@@ -48,64 +45,7 @@ trait Archive {
   def explode(dir: File): Unit
 
   /**
-   * Add the file to the archive using the path as a pointer
-   * to a directory within the archive.
-   */
-   def add(f: File, path: String): Unit
-}
-
-import java.io.{BufferedOutputStream, FileOutputStream}
-
-import org.apache.commons.io.IOUtils
-
-/**
- *
- * @author scox
- */
-class Zip(zipFile: ZipFile) extends Archive {
-  import scala.collection.JavaConversions._
-
-  def entryNames(pattern: Pattern): List[String] = 
-    entryNames.filter {e => pattern.matcher(e).matches}
-
-  def entryNames: List[String] = zipFile.entries.map {_.getName}.toList
-
-  def entryAsString(
-    name: String
-    , encoding: String = Zip.DefaultEncoding
-  ): String = {
-    val entry = zipFile.getEntry(name);
-    val in = zipFile.getInputStream(entry);
-    try {
-      IOUtils.toString(in, encoding)
-    } finally {
-      try { in.close } catch { case e:Exception => /* TODO: Log */ e.printStackTrace}
-    }
-  }
-
-  def explode(dir: File): Unit = {
-    require(!dir.exists || dir.isDirectory);
-
-    val entries = zipFile.entries;
-    entries.foreach {e =>
-      val newFile = new File(dir, e.getName);
-      if (e.isDirectory) {
-        newFile.mkdirs();
-      } else {
-        val out = new BufferedOutputStream(new FileOutputStream(newFile));
-        try {
-          IOUtils.copy(zipFile.getInputStream(e), out);
-        } finally {
-          out.close();
-        }
-      }
-    }
-  }
-
-  def add(f: File, path: String = "."): Unit = {
-  }
-}
-
-object Zip {
-  val DefaultEncoding = System.getProperty("file.encoding")
+  * The underlying file
+  */
+  def file: File
 }
